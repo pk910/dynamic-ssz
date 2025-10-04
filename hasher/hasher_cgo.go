9ff23b63fa54489dd1c7c899f5f029394a3fba38
pk910 <philipp@pk910.de>
2025-10-04 23:37:36 +0200

    add `nohashtree` build tag to exclude `OffchainLabs/hashtree` from builds

--- hasher/hasher_cgo.go
+++ hasher/hasher_cgo.go
@@ -1,8 +1,8 @@
 // Copyright (c) 2025 pk910
 // SPDX-License-Identifier: Apache-2.0
 // This file is part of the dynamic-ssz library.
-//go:build cgo
-// +build cgo
+//go:build cgo && !nohashtree
+// +build cgo,!nohashtree
 
 package hasher
 
