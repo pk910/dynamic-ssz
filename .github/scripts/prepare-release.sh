ef8fa02ec873dbdaa5a679e869745dc15f1682d6
pk910 <philipp@pk910.de>
2025-10-19 01:29:42 +0200

    fix `prepare-release.sh`

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -19,14 +19,14 @@ find . -name "go.mod" -not -path "./vendor/*" | while read -r go_mod_file; do
     # Skip if it's the root go.mod (where it's the module declaration)
     if grep -E "(require\s+github.com/pk910/dynamic-ssz|^\s+github.com/pk910/dynamic-ssz)" "$go_mod_file" && [ "$go_mod_file" != "./go.mod" ]; then
         # Update single-line require statements
-        sed -i "s|^require github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+[^[:space:]]*|require github.com/pk910/dynamic-ssz $new_version|g" "$go_mod_file"
+        sed -i "s|^require github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+[^[:space:]]*|require github.com/pk910/dynamic-ssz v$new_version|g" "$go_mod_file"
         
         # Update multi-line require block entries (lines that start with whitespace/tab)
-        sed -i "s|^\(\s\+\)github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+[^[:space:]]*|\1github.com/pk910/dynamic-ssz $new_version|g" "$go_mod_file"
+        sed -i "s|^\(\s\+\)github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+[^[:space:]]*|\1github.com/pk910/dynamic-ssz v$new_version|g" "$go_mod_file"
         
         # Handle pseudo-version format (v0.0.0-00010101000000-000000000000)
-        sed -i "s|^require github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+-[a-f0-9]\+|require github.com/pk910/dynamic-ssz $new_version|g" "$go_mod_file"
-        sed -i "s|^\(\s\+\)github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+-[a-f0-9]\+|\1github.com/pk910/dynamic-ssz $new_version|g" "$go_mod_file"
+        sed -i "s|^require github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+-[a-f0-9]\+|require github.com/pk910/dynamic-ssz v$new_version|g" "$go_mod_file"
+        sed -i "s|^\(\s\+\)github.com/pk910/dynamic-ssz v[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+-[a-f0-9]\+|\1github.com/pk910/dynamic-ssz v$new_version|g" "$go_mod_file"
         
         echo "Updated $go_mod_file"
         
